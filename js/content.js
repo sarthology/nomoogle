6953ab93c37cdb1687fada5af5b955ec43450504
Sarthak Sharma <7lovesharma7@gmail.com>
2018-12-08 13:32:45 +0530

    Domain Specific Suggestion added

--- js/content.js
+++ js/content.js
@@ -1,4 +1,12 @@
     let currentUrl = window.location.href;
     if(currentUrl.indexOf("google.com") > -1){
-        $("body").append("<div style='position:absolute;width: 70%;border: 1px solid #e3e3e3;z-index: 1000;left: 15%;top: 5%;padding: 10px;background: white;'><p> Stop using Google you fool. </p></div>") ;
+        $("body").append(`<div class='stopMessage'>
+                            <div class='shallNotPass'></div>
+                            <p class='messageText'> You shall not pass!!</p>
+                            <p class="closeButton">x</p>
+                        </div>`) ;
     }
+
+$(".closeButton").click(function (e) { 
+    $('.stopMessage').slideUp();
+});
