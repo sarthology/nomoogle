6375f0df28cbe3e9875d6f099adef17a09b6f265
Sarthak Sharma <7lovesharma7@gmail.com>
2018-12-11 18:03:57 +0530

    Code added for all google products

--- js/content.js
+++ js/content.js
@@ -3,17 +3,18 @@ let currentTabInfo = {
     "title": $(document).find("title").text()
 }
 
-$(".closeButton").click(function (e) {
-    $('.stopMessage').hide();
-});
 
 chrome.runtime.sendMessage(currentTabInfo, function (response) {
     if (response) {
         $("body").append(`<div class='stopMessage'>
-                            <div class='shallNotPass'></div>
-                            <p class='messageText'> You shall not pass!!</p>
-                            <p class="closeButton">x</p>
-                            <p class="extraInfo">We have found ${response.data.length} alertnative for ${response.title}</p>
-                        </div>`);
+        <div class='shallNotPass'></div>
+        <p class='messageText'> You shall not pass!!</p>
+        <p class="closeButton">x</p>
+        <p class="extraInfo">We have found ${response.data.length} alertnative for ${response.title}</p>
+        </div>`);
+
+        $(".closeButton").click(function (e) {
+            $('.stopMessage').hide();
+        });
     }
 });
