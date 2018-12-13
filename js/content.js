4dff3a335d02bcc20a4b6ed107173cae7d562cb8
Sarthak Sharma <7lovesharma7@gmail.com>
2018-12-13 15:36:42 +0530

    Settings added

--- js/content.js
+++ js/content.js
@@ -5,16 +5,29 @@ let currentTabInfo = {
 
 
 chrome.runtime.sendMessage(currentTabInfo, function (response) {
-    if (response) {
-        $("body").append(`<div class='stopMessage'>
-        <div class='shallNotPass'></div>
-        <p class='messageText'> You shall not pass!!</p>
-        <p class="closeButton">x</p>
-        <p class="extraInfo">We have found ${response.data.length} alertnative for ${response.title}</p>
-        </div>`);
+    chrome.storage.sync.get({
+        strictModeEnabled: false
+      }, function(items) { 
+        if (items.strictModeEnabled) {
+            $("body").html(`<div>
+            <div class='shallNotPass'></div>
+            <p class='messageText'> You shall not pass!!</p>
+            <p class="extraInfo">We have found ${response.data.length} alertnative for ${response.title}</p>
+            </div>`);
+        }
+        else{
+            $("body").append(`<div class='stopMessage'>
+            <div class='shallNotPass'></div>
+            <p class='messageText'> You shall not pass!!</p>
+            <p class="closeButton">x</p>
+            <p class="extraInfo">We have found ${response.data.length} alertnative for ${response.title}</p>
+            </div>`);
+    
+            $(".closeButton").click(function (e) {
+                $('.stopMessage').hide();
+            });
+        }
 
-        $(".closeButton").click(function (e) {
-            $('.stopMessage').hide();
-        });
-    }
+      });
+   
 });
---
+++ js/options.js
@@ -0,0 +1,21 @@
+$("#enableRedirects").click((e)=>{ 
+    chrome.storage.sync.set({
+        redirectEnabled: e.currentTarget.checked
+      });    
+})
+
+$("#enableStrictMode").click((e)=>{        
+    chrome.storage.sync.set({
+        strictModeEnabled: e.currentTarget.checked
+      });    
+})
+
+document.addEventListener('DOMContentLoaded',()=>{
+    chrome.storage.sync.get({
+        redirectEnabled: false,
+        strictModeEnabled: false
+      }, function(items) {          
+        $("#enableRedirects").prop('checked', items.redirectEnabled);
+        $("#enableStrictMode").prop('checked', items.strictModeEnabled);
+      });
+});
